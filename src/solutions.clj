d7e95f8faf33eee46ecea57cb61519f57e410457
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-10-23 19:26:10 -0300

    Importing a module to convert JSON to EDN

--- src/solutions.clj
+++ src/solutions.clj
@@ -1,6 +1,10 @@
 (ns solutions
-  (:require [clojure.spec.alpha :as s])
-  (:use [invoice-spec]))
+  (:require [clojure.spec.alpha :as s]
+            [tupelo.core :as t])
+  (:use [invoice-spec]
+        [clojure.set :only rename-keys]
+        [clojure.test :only deftest is])
+)
 
 (def invoice (clojure.edn/read-string (slurp "invoice.edn")))
 
@@ -27,9 +31,31 @@
     (and condition2 (not condition1))
     ))
 
+;; SOLUTION TO PROBLEM 1
+
 (defn iva-xor-ret-fuente
   [inv]
   (->> (:invoice/items inv)
        (filter #(xor (= 19 (get-iva %)) (= 1 (get-ret_fuente %))))
        )
   )
+
+;; SOLUTION TO PROBLEM 2
+;; Apparent bug
+;; while other defined specs work, with (s/get-spec ::invoice) we get nil, while, for example, (s/get-spec :invoice-item/price) returns an object
+
+;; (println (s/valid? ::invoice invoice))
+
+;; (s/valid? ::invoice invoice) => Execution error at solutions/eval2042 (form-init8543560321450255420.clj:1).
+;; Unable to resolve spec: :solutions/invoice
+
+(def invoice-to-validate (t/json->edn (slurp "invoice.json")))
+
+(defn json-invoice-to-map
+  [input-file]
+  (let [invoice (:invoice (t/json->edn (slurp input-file)))]
+    invoice ;; to-solve
+    )
+  )
+
+;; SOLUTION TO PROBLEM 3
