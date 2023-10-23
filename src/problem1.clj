5a665cb9b50282be3dc69e7a694d278a141c66ba
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-10-23 16:33:23 -0300

    Validating item conditions using xor

--- src/problem1.clj
+++ src/problem1.clj
@@ -8,19 +8,28 @@
   [item]
   (->> (:taxable/taxes item)
        (filter #(= (:tax/category %) :iva))
-       (map :tax/rate))
+       (map :tax/rate)
+       (first))
   )
 
 (defn get-ret_fuente
   [item]
   (->> (:retentionable/retentions item)
        (filter #(= (:retention/category %) :ret_fuente))
-       (map :retention/rate))
+       (map :retention/rate)
+       (first))
   )
+
+(defn xor
+  [condition1 condition2]
+  (or
+    (and condition1 (not condition2))
+    (and condition2 (not condition1))
+    ))
 
 (defn iva-xor-ret-fuente
   [inv]
   (->> (:invoice/items inv)
-       (filter )
+       (filter #(xor (= 19 (get-iva %)) (= 1 (get-ret_fuente %))))
        )
   )
