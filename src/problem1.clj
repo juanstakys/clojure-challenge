bcf03faecfed88d474defc738991b43a13e83025
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-10-23 16:17:32 -0300

    get-iva and get-ret_fuente functions

--- src/problem1.clj
+++ src/problem1.clj
@@ -1,1 +1,26 @@
-(ns problem1)
+(ns problem1
+  (:require [clojure.spec.alpha :as s])
+  (:use [invoice-spec]))
+
+(def invoice (clojure.edn/read-string (slurp "invoice.edn")))
+
+(defn get-iva
+  [item]
+  (->> (:taxable/taxes item)
+       (filter #(= (:tax/category %) :iva))
+       (map :tax/rate))
+  )
+
+(defn get-ret_fuente
+  [item]
+  (->> (:retentionable/retentions item)
+       (filter #(= (:retention/category %) :ret_fuente))
+       (map :retention/rate))
+  )
+
+(defn iva-xor-ret-fuente
+  [inv]
+  (->> (:invoice/items inv)
+       (filter )
+       )
+  )
