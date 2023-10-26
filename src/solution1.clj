1ecca90b8713559516afe9b797fe4969fee69598
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-10-26 15:30:03 -0300

    comment on first approach

--- src/solution1.clj
+++ src/solution1.clj
@@ -7,7 +7,7 @@
 (defn get-iva
   [item]
   (->> (:taxable/taxes item)
-       (filter #(= (:tax/category %) :iva))
+       (filter #(= (:tax/category %) :iva)) ;; I'm assuming that one item can only have one tax of the category :iva
        (map :tax/rate)
        (first))
   )
@@ -15,7 +15,7 @@
 (defn get-ret_fuente
   [item]
   (->> (:retentionable/retentions item)
-       (filter #(= (:retention/category %) :ret_fuente))
+       (filter #(= (:retention/category %) :ret_fuente)) ;; I'm assuming that one item can only have one retention of the category :ret_fuente
        (map :retention/rate)
        (first))
   )
