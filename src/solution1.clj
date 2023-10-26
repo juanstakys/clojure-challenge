5a2d2fadb3ba01119138a87da92a925022351ef5
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-10-26 16:28:12 -0300

    Second approach to problem 1 + some comments

--- src/solution1.clj
+++ src/solution1.clj
@@ -4,6 +4,7 @@
 
 (def invoice (clojure.edn/read-string (slurp "invoice.edn")))
 
+;; Approach 1: Use exclusive or to verify conditions (1.) and (2.)
 (defn get-iva
   [item]
   (->> (:taxable/taxes item)
@@ -34,4 +35,23 @@
        )
   )
 
+;; Approach 2: Filter IN all items with (iva 19 OR ret 1) and then OUT (iva 19 AND ret 1)
+
+(defn filter-items-iva-ret
+  [inv]
+  (->> (:invoice/items inv)
+       (filter #(or (= 19 (get-iva %))
+                    (= 1 (get-ret_fuente %))))
+       (filter #(not
+                  (and (= 19 (get-iva %))
+                       (= 1 (get-ret_fuente %)))))
+       )
+  )
+
+; Approach 1
+(println "Approach 1:")
 (clojure.pprint/pprint (iva-xor-ret-fuente invoice))
+
+; Approach 2
+(println "Approach 2:")
+(clojure.pprint/pprint (filter-items-iva-ret invoice))
